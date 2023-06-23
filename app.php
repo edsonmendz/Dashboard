72a713257fcdef405fb2b3a7fc742df3c4189264
edsonmendz <japaact@gmail.com>
2023-06-23 11:12:22 -0300

    Corrigi o erro em que faltava um $ no __set

--- app.php
+++ app.php
@@ -12,7 +12,7 @@ class Dashboard {
     }
 
     public function __set($atributo, $valor) {
-        $this->atributo = $valor;
+        $this->$atributo = $valor;
         return $this;
     }
 }
@@ -25,11 +25,59 @@ class Conexao {
 
     public function conectar() {
         try {
+            $conexao = new PDO(
+                "mysql:host=$this->host;dbname=$this->dbname",
+                "$this->user",
+                "$this->pass"
+            );
 
-        } catch(PDOException $e) {
+            $conexao->exec('set charset utf8');
+            return $conexao;
+        }
+         catch(PDOException $e) {
             echo 'p'.$e->getMessage().'p';
         }
-
+    
     }
 }
+
+
+//classe (model)
+
+class Bd {
+    private $conexao;
+    private $dashboard;
+
+    public function __construct(Conexao $conexao, Dashboard $dashboard) {
+        $this->conexao = $conexao->conectar();
+        $this->dashboard = $dashboard;
+    }
+
+    public function getNumeroVendas() {
+        $query = '
+            select
+                count(*) as numero_vendas
+            from
+                tb_vendas
+            where data_venda between :data_inicio and :data_fim';
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+        $stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+        $stmt->execute();
+        
+        return $stmt->fetch(PDO::FETCH_OBJ)->numero_vendas;
+    }
+}
+
+$dashboard = new Dashboard();
+$conexao = new Conexao();
+$dashboard->__set('data_inicio', '2018-8-01');
+$dashboard->__set('data_fim', '2018-8-31' );
+
+
+$bd = new Bd($conexao, $dashboard);
+
+$dashboard->__set('numeroVendas', $bd->getNumeroVendas());
+
+print_r($dashboard)
 ?>
